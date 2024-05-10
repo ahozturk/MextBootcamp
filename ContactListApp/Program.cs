93a429b6daa44c57a8dfc2d51a66a41d2a1e65d7
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-10 14:38:29 +0300

    Update

--- ContactListApp/Program.cs
+++ ContactListApp/Program.cs
@@ -19,59 +19,56 @@ string ReadConsoleWithMessage(string message)
     return Console.ReadLine();
 }
 
+void PrintPeople()
+{
+    foreach (var person in people)
+        Console.WriteLine(person);
+}
+
+void AddPerson()
+{
+    string name = ReadConsoleWithMessage("Please enter a name: ");
+    people.Add(name);
+    Console.WriteLine($"{name} is saved!");    
+}
+
+void DeletePerson()
+{
+    string name = ReadConsoleWithMessage("Please enter a name to delete: ");
+
+    people.Remove(name);
+    Console.WriteLine($"{name} is deleted!");
+}
+
+void UpdatePerson()
+{
+    string name = ReadConsoleWithMessage("Please enter a name to update: ");
+
+    int index = people.IndexOf(name);
+
+    string nameUpdated = ReadConsoleWithMessage("Please enter updated version: ");
+
+    people[index] = nameUpdated;
+
+    Console.WriteLine($"{name} is updated to {nameUpdated}!");
+    Console.WriteLine();
+}
+
 while (true)
 {
     PrintMenu();
 
-    // Console.WriteLine("Enter a selection:");
-    // string selection = Console.ReadLine();
-
     string selection = ReadConsoleWithMessage("Enter a selection:");
 
     if (selection == "1")
-    {
-        foreach (var person in people)
-        {
-            Console.WriteLine(person);
-        }
-    }
+        PrintPeople();
+
     else if (selection == "2")
-    {
-        // Console.WriteLine("Please enter a name: ");
-        // string name = Console.ReadLine();
-
-        string name = ReadConsoleWithMessage("Please enter a name: ");
-
-        people.Add(name);
-        Console.WriteLine($"{name} is saved!");
-    }
+        AddPerson();
+        
     else if (selection == "3")
-    {
-        // Console.WriteLine("Please enter a name to delete: ");
-        // string name = Console.ReadLine();
+        DeletePerson();        
 
-        string name = ReadConsoleWithMessage("Please enter a name to delete: ");
-
-        people.Remove(name);
-        Console.WriteLine($"{name} is deleted!");
-    }
     else if (selection == "4")
-    {
-        // Console.WriteLine("Please enter a name to update: ");
-        // string name = Console.ReadLine();
-
-        string name = ReadConsoleWithMessage("Please enter a name to update: ");
-
-        int index = people.IndexOf(name);
-
-        // Console.WriteLine("Please enter updated version: ");
-        // string nameUpdated = Console.ReadLine();
-
-        string nameUpdated = ReadConsoleWithMessage("Please enter updated version: ");
-
-        people[index] = nameUpdated;
-
-        Console.WriteLine($"{name} is updated to {nameUpdated}!");
-        Console.WriteLine();
-    }
+        UpdatePerson();
 }
