4aafd9137c8df64334ce29d29f26791f172f45de
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-10 13:06:22 +0300

    Contact List App Added

---
+++ ContactListApp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+List<string> people = new();
+
+
+
+//Add Person
+people.Add("Alice"); //0
+people.Add("Hakan"); //1
+people.Add("Bob"); //2
+
+//List People
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+}
+Console.WriteLine("**********");
+//Remove Person
+people.Remove("Bob");
+
+//List People
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+}
+Console.WriteLine("**********");
+
+//Update Person
+people[0] = "Alice Smith";
+people[1] = "Hakan Öztürk";
+
+//List People
+foreach (string person in people)
+{
+    Console.WriteLine(person);
+}
