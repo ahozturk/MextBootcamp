e72c3c3d157b0ca1cb8aa076ee33cf33ca121425
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-10 13:48:39 +0300

    Added - Basic CRUD Operations

--- ContactListApp/Program.cs
+++ ContactListApp/Program.cs
@@ -1,35 +1,50 @@
 ﻿
-List<string> people = new();
-
-
-
-//Add Person
-people.Add("Alice"); //0
-people.Add("Hakan"); //1
-people.Add("Bob"); //2
-
-//List People
-foreach (string person in people)
+List<string> people = new()
 {
-    Console.WriteLine(person);
+    "Müberra",
+    "Şeyda",
+};
+
+Console.WriteLine("1 - List People");
+Console.WriteLine("2 - Add Person");
+Console.WriteLine("3 - Remove Person");
+Console.WriteLine("4 - Update Person");
+
+string selection = Console.ReadLine();
+
+if (selection == "1")
+{
+    foreach (var person in people)
+    {
+        Console.WriteLine(person);
+    }
 }
-Console.WriteLine("**********");
-//Remove Person
-people.Remove("Bob");
-
-//List People
-foreach (string person in people)
+else if (selection == "2")
 {
-    Console.WriteLine(person);
+    Console.WriteLine("Please enter a name: ");
+    string name = Console.ReadLine();
+    people.Add(name);
+    Console.WriteLine($"{name} is saved!");
 }
-Console.WriteLine("**********");
-
-//Update Person
-people[0] = "Alice Smith";
-people[1] = "Hakan Öztürk";
-
-//List People
-foreach (string person in people)
+else if (selection == "3")
 {
-    Console.WriteLine(person);
+    Console.WriteLine("Please enter a name to delete: ");
+    string name = Console.ReadLine();
+    people.Remove(name);
+    Console.WriteLine($"{name} is deleted!");
+}
+else if (selection == "4")
+{
+    Console.WriteLine("Please enter a name to update: ");
+    string name = Console.ReadLine();
+
+    int index = people.IndexOf(name);
+
+    Console.WriteLine("Please enter updated version: ");
+    string nameUpdated = Console.ReadLine();
+
+    people[index] = nameUpdated;
+
+    Console.WriteLine($"{name} is updated to {nameUpdated}!");
+    Console.WriteLine();
 }
