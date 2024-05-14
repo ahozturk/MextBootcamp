60a317cd1517388f2ab03082ec02bb470748ec54
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-14 14:26:53 +0300

    Update

--- CustomerApp/Customer.cs
+++ CustomerApp/Customer.cs
@@ -9,4 +9,21 @@ public class Customer
     public string Mail { get; set; }
     public string Address { get; set; } //ToDo: Make it in a Model
     public Gender Gender { get; set; }
+
+    //8
+
+    public Customer(string name, string surname)
+    {
+        //""
+        if (name.Replace(" ", "").Length == 0)
+            Name = "Unknown";
+        else
+            Name = name;
+
+        if (surname.Length == 0)
+            Surname = "Unknown";
+        else
+            Surname = surname;
+    }
+
 }
--- CustomerApp/Program.cs
+++ CustomerApp/Program.cs
@@ -1,62 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region Main Program
+
+// using CustomerApp;
+
+// var customer = CreateCustomer();
+
+// Console.WriteLine($"Customer: {customer.Name} {customer.Surname} {customer.PhoneNumber} {customer.Mail} {customer.Address} {customer.Gender}");
+
+
+
+// Customer CreateCustomer()
+// {
+//     Console.WriteLine("Please enter the customer name:");
+//     var name = Console.ReadLine();
+
+//     Console.WriteLine("Please enter the customer surname:");
+//     var surname = Console.ReadLine();
+
+//     Console.WriteLine("Please enter the customer phone number:");
+//     var phoneNumber = Console.ReadLine();
+
+//     Console.WriteLine("Please enter the customer mail:");
+//     var mail = Console.ReadLine();
+
+//     Console.WriteLine("Please enter the customer address:");
+//     var address = Console.ReadLine();
+
+//     Console.WriteLine("Please enter the customer gender: (Female, Male, Unknown, NotPreferToSay, Other)");
+//     var genderString = Console.ReadLine(); //"Male"
+
+//     Gender gender = GetGenderFromString(genderString);
+
+//     Customer customer = new Customer()
+//     {
+//         Name = name,
+//         Surname = surname,
+//         PhoneNumber = phoneNumber,
+//         Mail = mail,
+//         Address = address,
+//         Gender = gender,
+//     };
+
+//     return customer;
+// }
+
+// Gender GetGenderFromString(string genderString)
+// {
+//     //male -> male
+//     genderString = genderString.ToLower();
+
+//     if (genderString == "female")
+//         return Gender.Female;
+
+//     else if (genderString == "male")
+//         return Gender.Male;
+
+//     else if (genderString == "unknown")
+//         return Gender.Unknown;
+
+//     else if (genderString == "notprefertosay")
+//         return Gender.NotPreferToSay;
+
+//     return Gender.Other;
+// }
+#endregion
+
+
 using CustomerApp;
 
-Console.WriteLine("Hello, World!");
+var newCustomer = new Customer("      ", "Öztürk");
 
-var customer = CreateCustomer();
-
-Console.WriteLine($"Customer: {customer.Name} {customer.Surname} {customer.PhoneNumber} {customer.Mail} {customer.Address} {customer.Gender}");
-
-Customer CreateCustomer()
-{
-    Console.WriteLine("Please enter the customer name:");
-    var name = Console.ReadLine();
-
-    Console.WriteLine("Please enter the customer surname:");
-    var surname = Console.ReadLine();
-
-    Console.WriteLine("Please enter the customer phone number:");
-    var phoneNumber = Console.ReadLine();
-
-    Console.WriteLine("Please enter the customer mail:");
-    var mail = Console.ReadLine();
-
-    Console.WriteLine("Please enter the customer address:");
-    var address = Console.ReadLine();
-
-    Console.WriteLine("Please enter the customer gender: (Female, Male, Unknown, NotPreferToSay, Other)");
-    var genderString = Console.ReadLine(); //"Female"
-
-    Gender gender = GetGenderFromString(genderString);
-
-    Customer customer = new()
-    {
-        Name = name,
-        Surname = surname,
-        PhoneNumber = phoneNumber,
-        Mail = mail,
-        Address = address,
-        Gender = gender,
-    };
-
-    return customer;
-}
-
-Gender GetGenderFromString(string genderString)
-{
-    genderString = genderString.ToLower();
-
-    if (genderString == "female")
-        return Gender.Female;
-
-    else if (genderString == "male")
-        return Gender.Male;
-
-    else if (genderString == "unknown")
-        return Gender.Unknown;
-
-    else if (genderString == "notprefertosay")
-        return Gender.NotPreferToSay;
-
-    return Gender.Other;
-}
+Console.WriteLine($"Customer: {newCustomer.Name} - {newCustomer.Surname}");
