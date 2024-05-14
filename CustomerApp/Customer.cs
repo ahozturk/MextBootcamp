01c511898375d11a14600426deae7b4baebd62ed
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-14 10:51:31 +0300

    Update

---
+++ CustomerApp/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomerApp;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Mail { get; set; }
+    public string Address { get; set; } //ToDo: Make it in a Model
+    public Gender Gender { get; set; }
+}
---
+++ CustomerApp/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerApp;
+
+public enum Gender
+{
+    Female = 0,
+    Male = 1,
+    Unknown = 2,
+    NotPreferToSay = 3,
+}
