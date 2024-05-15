faf68f4d92c57287c6e2493ff045304eaef955ad
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-15 10:31:11 +0300

    Update

--- Onedemio/Program.cs
+++ Onedemio/Program.cs
@@ -1,57 +1,36 @@
-﻿
-#region Question
-//1) Merdivenden inerken korkuluğa tutunma
-//Evet (Kış) - 1
-//Hayır (Yaz) - 0
-
-//2) Yükseklik korkusu
-//Evet (Kış) - 1
-//Hayır (Yaz) - 0
-
-//3) Sporu sevmek
-//Evet (Yaz) - 0
-//Hayır (Kış) - 1
-
-//Değerlendirme:
-//Puan 1<: Kış
-//Puan 0: Yaz
-#endregion
-
-using Onedemio;
+﻿using Onedemio;
 
 int totalPoint = 0;
 
-// Console.WriteLine("Merdivenden inerken korkuluğa tutunur musun? (Evet/Hayır)");
-
-// string answer1 = Console.ReadLine();
-
-// if (answer1 == "Evet")
-//     totalPoint += 1;
-
-
-// Console.WriteLine("Yükseklik korkun var mı? (Var/Yok)");
-
-// string answer2 = Console.ReadLine();
-
-// if (answer2 == "Var")
-//     totalPoint += 1;
-
-
-// Console.WriteLine("Spor yapmayı sever misin? (Evet/Hayır)");
-
-// string answer3 = Console.ReadLine();
-
-// if (answer3 == "Hayır")
-//     totalPoint += 1;
-
-SurveyQuestion surveyQuestion = new()
+List<SurveyQuestion> surveyQuestions = new()
 {
-    Question = "Spor yapmayı sever misin?",
-    CorrectAnswer = "Hayır",
-    WrongAnswer = "Evet"
+    new()
+    {
+        Question = "Merdivenden inerken korkuluğa tutunur musun?",
+        CorrectAnswer = "Evet",
+        WrongAnswer = "Hayır"
+    },
+    new()
+    {
+        Question = "Spor yapmayı sever misin?",
+        CorrectAnswer = "Hayır",
+        WrongAnswer = "Evet"
+    },
+    new()
+    {
+        Question = "Yükseklikten korkar mısın?",
+        CorrectAnswer = "Evet",
+        WrongAnswer = "Hayır"
+    }
 };
 
-totalPoint += WriteQuestionAndGetAnswer(surveyQuestion);
+
+foreach (var surveyQuestion in surveyQuestions)
+{
+    totalPoint += WriteQuestionAndGetAnswer(surveyQuestion);
+}
+
+
 
 int WriteQuestionAndGetAnswer(SurveyQuestion surveyQuestion)
 {
