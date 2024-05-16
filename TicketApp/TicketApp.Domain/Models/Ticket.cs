d7fea1b1cc2ae31a9e32215d4bc30637ed926c5f
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-16 13:46:04 +0300

    Update

---
+++ TicketApp/TicketApp.Domain/Models/Ticket.cs
@@ -0,0 +1,8 @@
+﻿namespace TicketApp.Domain;
+
+public class Ticket
+{
+    public string Id { get; set; }
+    public string OwnerNameSurname { get; set; }
+    public decimal Price { get; set; }
+}
---
+++ TicketApp/TicketApp.Domain/Services/TicketService.cs
@@ -0,0 +1,17 @@
+﻿namespace TicketApp.Domain;
+
+public class TicketService
+{
+    public void SaveTicketToTextFile(string ownerNameSurname, decimal price)
+    {
+        Ticket ticket = new()
+        {
+            OwnerNameSurname = ownerNameSurname,
+            Price = price
+        };
+
+        string ticketInfo = $"{ticket.Id},{ticket.OwnerNameSurname},{ticket.Price}";
+        //1,Hakan Öztürk,100
+        File.AppendAllText("/Users/hakan/Documents/GitHub/MextBootcamp/TicketApp/TicketApp.Domain/ticket.txt", ticketInfo);
+    }
+}
