77b2c10e6485d553a5a6ae1c82804aef59eadb9b
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-16 13:58:09 +0300

    Update

--- TicketApp/TicketApp.Domain/Services/TicketService.cs
+++ TicketApp/TicketApp.Domain/Services/TicketService.cs
@@ -4,13 +4,16 @@ public class TicketService
 {
     public void SaveTicketToTextFile(string ownerNameSurname, decimal price)
     {
+        Random random = new();
+
         Ticket ticket = new()
         {
+            Id = $"{random.Next(1, 1000)}",
             OwnerNameSurname = ownerNameSurname,
             Price = price
         };
 
-        string ticketInfo = $"{ticket.Id},{ticket.OwnerNameSurname},{ticket.Price}";
+        string ticketInfo = $"{ticket.Id},{ticket.OwnerNameSurname},{ticket.Price}\n";
         //1,Hakan Öztürk,100
         File.AppendAllText("/Users/hakan/Documents/GitHub/MextBootcamp/TicketApp/TicketApp.Domain/ticket.txt", ticketInfo);
     }
--- TicketApp/TicketApp.Presentation/Program.cs
+++ TicketApp/TicketApp.Presentation/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TicketApp.Domain;
+
+TicketService ticketService = new();
+
+//1:
+// Ticket ticket = new()
+// {
+//     OwnerNameSurname = "Hakan Öztürk",
+//     Price = 100
+// };
+
+// ticketService.SaveTicketToTextFile(ticket);
+
+ticketService.SaveTicketToTextFile("Hakan Öztürk", 100);
+
+Console.WriteLine("Ticket saved to text file.");
