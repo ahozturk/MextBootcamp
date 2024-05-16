5d74b650b4e518038b942522280806d53184e3c3
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-16 10:20:39 +0300

    Update

---
+++ Custify/Custify.Domain/Services/CustomerService.cs
@@ -0,0 +1,16 @@
+﻿namespace Custify.Domain;
+
+public class CustomerService
+{
+    static List<Customer> _customers = new();
+
+    public void Add(string name)
+    {
+        Random random = new();
+
+        new Customer()
+        {
+            Id = random.Next(1_000, 1_000_000),
+        }
+    }
+}
