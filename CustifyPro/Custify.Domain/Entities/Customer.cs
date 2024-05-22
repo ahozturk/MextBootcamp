a845a0921635b48f648ec00f822d7068f000cb9a
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-22 10:01:42 +0300

    Update

--- CustifyPro/Custify.Domain/Entities/Customer.cs
+++ CustifyPro/Custify.Domain/Entities/Customer.cs
@@ -1,12 +1,14 @@
-﻿namespace Custify.Domain.Entities;
+﻿using Custify.Domain.Enums;
+
+namespace Custify.Domain.Entities;
 
 public class Customer
 {
-    // public Guid Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Email { get; set; }
     public string PhoneNumber { get; set; }
     public string Address { get; set; }
-    public string Gender { get; set; }
+    public Gender Gender { get; set; }
 }
--- CustifyPro/Custify.Domain/Enums/Gender.cs
+++ CustifyPro/Custify.Domain/Enums/Gender.cs
@@ -1,6 +1,10 @@
-﻿namespace Custify.Domain;
+﻿namespace Custify.Domain.Enums;
 
-public class Gender
+public enum Gender
 {
-
+    Female = 1,
+    Male = 2,
+    Unknown = 3,
+    PreferNotToSay = 4,
+    Other = 5,
 }
