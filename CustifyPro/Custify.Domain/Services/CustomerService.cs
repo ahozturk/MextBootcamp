5a20b83b2ab034a15bf5a93a4b62b62a07ee71e2
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-05-22 10:11:03 +0300

    Update

--- CustifyPro/Custify.Domain/Services/CustomerService.cs
+++ CustifyPro/Custify.Domain/Services/CustomerService.cs
@@ -1,6 +1,34 @@
-﻿namespace Custify.Domain;
+﻿using Custify.Domain.Entities;
+
+namespace Custify.Domain;
 
 public class CustomerService
 {
+    private static List<Customer> customers = new();
 
+    public List<Customer> GetAll()
+    {
+        return customers;
+    }
+
+    public void Add(Customer customer) // Will return result
+    {
+        customers.Add(customer);
+    }
+
+    public void Update(Customer updateCustomer)
+    {
+        foreach (var customer in customers)
+        {
+            if (customer.Id == updateCustomer.Id)
+            {
+                customer.Name = updateCustomer.Name;
+                customer.Surname = updateCustomer.Surname;
+                customer.Email = updateCustomer.Email;
+                customer.PhoneNumber = updateCustomer.PhoneNumber;
+                customer.Address = updateCustomer.Address;
+                break;
+            }
+        }
+    }
 }
