05087ee651d353140bfc2333ce46436d50e722ab
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-06-25 12:43:05 +0300

    Update

--- EFCoreLearning/EFCore.Console/Program.cs
+++ EFCoreLearning/EFCore.Console/Program.cs
@@ -10,13 +10,38 @@ var student1 = new Student()
     LastName = "Doe"
 };
 
+var student2 = new Student()
+{
+    Id = Guid.NewGuid(),
+    FirstName = "Hakan",
+    LastName = "Öztürk"
+};
+
 dbContext.Students.Add(student1);
+dbContext.Students.Add(student2);
 
 dbContext.SaveChanges();
 
 var students = dbContext.Students.ToList();
 
+
+
+var deletedStudent = dbContext.Students.FirstOrDefault(x => x.FirstName == "Hakan");
+
+dbContext.Students.Remove(deletedStudent);
+dbContext.SaveChanges();
+
+// foreach (var student in students)
+// {
+//     if (student.FirstName == "Hakan")
+//     {
+//         dbContext.Students.Remove(student);
+//         dbContext.SaveChanges();
+//         Console.WriteLine("Hakan removed from the database");
+//     }
+// }
+
+students = dbContext.Students.ToList();
+
 foreach (var student in students)
-{
     Console.WriteLine($"Student: {student.FirstName} {student.LastName}");
-}
