4d661ded3a5886c6c5566a51650f2d8640277af5
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-01 10:12:07 +0300

    Update

---
+++ HackerRankProblems/SolveMeFirst/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+
+    static int solveMeFirst(int a, int b) { 
+      int result = a + b;
+      return result;
+    }
+
+    static void Main(String[] args) {
+        int val1 = Convert.ToInt32(Console.ReadLine());
+        int val2 = Convert.ToInt32(Console.ReadLine());
+        int sum = solveMeFirst(val1,val2);
+        Console.WriteLine(sum);
+    }
+}      
