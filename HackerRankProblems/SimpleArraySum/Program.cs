f941917d02736e732ee3742fc810419b5b7258a6
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-01 11:09:03 +0300

    Update

---
+++ HackerRankProblems/SimpleArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿class Result
+{
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sumOfList = 0;
+        
+        foreach (int element in ar)
+        {
+            sumOfList += element;
+        }
+        
+        return sumOfList;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
