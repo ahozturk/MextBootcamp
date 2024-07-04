0207215f33bc7e69379252d49c97fe48438e5de0
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-04 14:21:42 +0300

    Update

---
+++ GPTClone/GPTClone.Domain/Common/EntityBase.cs
@@ -0,0 +1,8 @@
+﻿namespace GPTClone.Domain;
+
+public class EntityBase
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public string CreatedBy { get; set; }
+}
---
+++ GPTClone/GPTClone.Domain/Entities/Prompt.cs
@@ -0,0 +1,7 @@
+﻿namespace GPTClone.Domain;
+
+public class Prompt : EntityBase
+{
+    public string Text { get; set; } //"En ucuz makyaj malzemeleri nerededir?"
+    public string Response { get; set; } //"Gratis, Watsons, Rossmann, A101"
+}
