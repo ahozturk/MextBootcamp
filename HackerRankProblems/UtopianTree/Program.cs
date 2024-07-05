5d1ed017381ec4b07a7596994b23379befb4d95f
Hakan Öztürk <ahmethakanozturk34@gmail.com>
2024-07-05 16:11:20 +0300

    Update

---
+++ HackerRankProblems/UtopianTree/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int utopianTree(int n)
+    {
+        int treeLength = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (i % 2 == 0)
+                treeLength *= 2;
+            else if (i % 2 == 1)
+                treeLength += 1;
+        }
+        
+        return treeLength;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.utopianTree(n);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
